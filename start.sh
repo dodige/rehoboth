b7dbf0ceeb4680c47ab44f47b3b4118db63a71e6
dodige <dodige@gmail.com>
2018-03-26 21:14:53 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-cd /home/pi/PiModules/code/python/package
-python ./setup.py install
-
-cd /home/pi/PiModules/code/python/upspico/picofssd
-##RUN RUN chmod +x ./setup.py
-python ./setup.py install
-update-rc.d picofssd defaults
-update-rc.d picofssd enable
 
 sudo /etc/init.d/cron restart
 
