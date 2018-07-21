38b6f75301225e322597db3218931e8ffdc07c99
dodige <dodige@gmail.com>
2018-07-21 02:41:18 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,12 +8,14 @@ cd /home/pi
 
 /home/pi/updateModemState.sh&
 
-/home/pi/om2.py&
-
 uv4l -driver raspidisp
 export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
 route add -net 224.0.0.0 netmask 240.0.0.0 eth0
-avconv -f video4linux2  -re -i /dev/uv4l -f avi -an   udp://239.0.1.23:1234& 
+#avconv -f video4linux2  -re -i /dev/uv4l -f avi -an   udp://239.0.1.23:1234& 
+
+
+/home/pi/om2.py
+
 
 # sleep 30
 
